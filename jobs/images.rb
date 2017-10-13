36b92aa0cd3cdfa85ab97cc2b85b42a78c129bce
Sean <sb.rutherfurd@gmail.com>
2017-10-13 14:37:09 +1300

    Attempt 4. Change in how rand is used.

--- jobs/images.rb
+++ jobs/images.rb
@@ -1,13 +1,11 @@
 require 'Random'
 
-
-randomiser = Random.new(1234)
 imageList = ['/Worgen.jpeg', '/Draenei.jpeg', '/Forsaken.jpeg', '/Night Elven Malukah.jpeg', '/Protoss.png', '/Hydralisk.png']
 
 
 SCHEDULER.every '10s', :first_in => 0 do |job|
 	begin
-		images = imageList[randomiser.rand(imageList.length)]
+		images = imageList[rand(imageList.length)]
 		send_event('image_display', comments: images)
 		end
 	end
